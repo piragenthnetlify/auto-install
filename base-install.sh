8ac37f6c845f8c80f21e53e3b07d708fdc711f69
piragenthnetlify <88920285+piragenthnetlify@users.noreply.github.com>
2021-11-08 15:39:26 +0530

    Update base-install.sh

--- base-install.sh
+++ base-install.sh
@@ -4,7 +4,7 @@
 
 command sed -i '/cdrom/d' '/etc/apt/sources.list'
 command rm /etc/apt/sources.list
-command cp -r sources.list /etc/apt/sources.list
+command cp sources.list /etc/apt/
 command apt update && apt upgrade -y
 command apt install git sudo curl wget docker docker.io nano network-manager dhcpcd5 wireless-tools firmware-realtek linux-headers-generic build-essential dkms gdebi software-properties-common -y
 command git clone https://github.com/Mange/rtl8192eu-linux-driver
@@ -16,7 +16,7 @@ command echo -e "8192eu\n\nloop" | sudo tee /etc/modules
 command echo "options 8192eu rtw_power_mgnt=0 rtw_enusbss=0" | sudo tee /etc/modprobe.d/8192eu.conf
 command sudo update-grub; sudo update-initramfs -u
 command mv /etc/network/interfaces /etc/network/interfaces.backup
-command cp -r interfaces /etc/network/interfaces
+command cp interfaces /etc/network/
 command ifup wlx7cc2c611779b 
 command git clone https://github.com/oblique/create_ap
 command wget http://www.mobile-stream.com/beta/debian/10/easytether_0.8.9_amd64.deb
