f4ee1a7a94e3098287a802d56542458f2235ee59
root <root@home-server.home-server.local>
2021-12-15 07:23:27 -0800

    dsfae

---
+++ debian-server/docker-pull.sh
@@ -0,0 +1,15 @@
+docker pull oznu/unms
+docker pull linuxserver/webtop
+docker pull b4bz/homer
+docker pull debian
+docker pull ubuntu
+docker pull jaymoulin/jdownloader
+docker pull jlesage/jdownloader-2
+docker pull linuxserver/jellyfin
+docker pull linuxserver/nextcloud
+docker pull linuxserver/plex
+docker pull linuxserver/syncthing
+docker pull netdata/netdata
+docker pull oznu/guacamole
+docker pull oznu/homebridge
+
