409d71a9e9a13b3ea284d970926b29ece2784441
piragenthnetlify <88920285+piragenthnetlify@users.noreply.github.com>
2021-12-29 06:03:23 +0000

    change

--- debian-server/docker-pull.sh
+++ debian-server/docker-pull.sh
@@ -1,15 +1,19 @@
-docker pull oznu/unms
-docker pull linuxserver/webtop
-docker pull b4bz/homer
-docker pull debian
-docker pull ubuntu
-docker pull jaymoulin/jdownloader
-docker pull jlesage/jdownloader-2
+#docker pull oznu/unms
+#docker pull b4bz/homer
+#docker pull debian
+#docker pull ubuntu
+
+#docker pull netdata/netdata
+#docker pull oznu/homebridge
+
+
 docker pull linuxserver/jellyfin
 docker pull linuxserver/nextcloud
 docker pull linuxserver/plex
 docker pull linuxserver/syncthing
-docker pull netdata/netdata
-docker pull oznu/guacamole
-docker pull oznu/homebridge
+docker pull jaymoulin/jdownloader
+docker pull jlesage/jdownloader-2
 
+#supperate
+docker pull linuxserver/webtop
+docker pull oznu/guacamole
