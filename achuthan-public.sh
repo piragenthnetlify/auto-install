7501e888bb8b5d8c53d15d4f3460ebb8085566fa
piragenthnetlify <piragenthtechytel@gmail.com>
2022-02-23 12:15:35 -0800

    fsadf

--- achuthan-public.sh
+++ achuthan-public.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -i
 sudo apt update
 sudo apt upgrade -y
+sudo apt install bmon curl wget gdebi neofetch cmatrix -y
 wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb 
 echo 'y' | gdebi cloudflared-linux-amd64.deb
 cloudflared tunnel login
