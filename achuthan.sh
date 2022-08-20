72e7b452698254cfeaafc3e72e89531bb5a6b683
piragenthnetlify <88920285+piragenthnetlify@users.noreply.github.com>
2022-08-20 08:42:30 -0500

    fasdfasdf

--- achuthan.sh
+++ achuthan.sh
@@ -1,7 +1,17 @@
 echo atchuthan | sudo -S 
 sudo apt update 
-sudo apt upgrade -y
+#sudo apt upgrade -y
 sudo apt install gdebi wget -y
 wget -o easytether.deb http://www.mobile-stream.com/beta/ubuntu/20.04/easytether_0.8.9_amd64.deb
 sudo gdebi easytether.deb
-sudo apt install vsftpd bmon curl gnupg2 apt-transport-https ca-certificates snapd software-properties-common docker.io -y
+sudo easytether-usb 
+
+echo "Disconnect the ethernet"
+sudo apt install vsftpd bmon curl gnupg2 apt-transport-https ca-certificates snapd software-properties-common docker.io smbd -y
+
+sudo systemctl restart systemd-networkd
+sudo systemctl restart systemd-networkd
+sudo systemctl status systemd-networkd
+sudo docker pull linuxserver/jellyfin
+sudo docker pull linuxserver/plex
+sudo docker pull linuxserver/emby
