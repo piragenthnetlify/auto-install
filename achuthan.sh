d5e2135ecaba9ce38b35c84fcdc01591954f654a
piragenth-ltfb <111213108+piragenth-ltfb@users.noreply.github.com>
2022-08-20 08:34:12 -0500

    piragenth

--- achuthan.sh
+++ achuthan.sh
@@ -1,22 +1,7 @@
 echo atchuthan | sudo -S 
 sudo apt update 
 sudo apt upgrade -y
-sudo apt install vsftpd bmon curl gnupg2 apt-transport-https ca-certificates snapd software-properties-common -y
-sudo systemctl start vsftpd 
-sudo systemctl enable vsftpd
-docker pull jwilder/nginx-proxy
-docker pull nginx
-docker pull jrcs/letsencrypt-nginx-proxy-companion
-docker pull mariadb
-docker pull nextcloud
-docker pull willfarrell/autoheal
-docker pull dorowu/ubuntu-desktop-lxde-vnc:bionic
-snap install nextcloud
-sudo snap set nextcloud ports.http=81
-mkdir docker-ubuntu
-wget https://agiledevart.github.io/Dockerfile.lubuntu -P $(pwd)/docker-ubuntu
-wget https://agiledevart.github.io/Dockerfile.ubuntu
-docker build -t docker-ubuntu .
-docker build -t docker-lubuntu $(pwd)/docker-ubuntu/
-sudo apt update 
-sudo apt upgrade -y
+sudo apt install gdebi wget -y
+wget -o easytether.deb http://www.mobile-stream.com/beta/ubuntu/20.04/easytether_0.8.9_amd64.deb
+sudo gdebi easytether.deb
+sudo apt install vsftpd bmon curl gnupg2 apt-transport-https ca-certificates snapd software-properties-common docker.io -y
